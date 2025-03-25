717cdfed481268ab91219257f247ad24c19addd1
Marc Coët <github-h9s2@m4rr.co>
2025-03-25 21:03:08 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,41 @@
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+import cloudflare from "@astrojs/cloudflare";
+import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel";
+import { LOCAL_BUILD, PREFERRED_HOSTING, NETLIFY_BUILD, CLOUDFLARE_BUILD, VERCEL_BUILD } from "./astro.config.env";
+
+const cloudflareOptions = {
+  platformProxy: { enabled: true, configPath: 'wrangler.jsonc', experimentalJsonConfig: true }
+}
+
+// NOTE: Cloudflare workaround for Error:
+// `Failed to publish your Function. Got error: Uncaught ReferenceError: MessageChannel is not defined`
+// https://github.com/withastro/adapters/pull/436#issuecomment-2525190557
+// https://github.com/withastro/astro/issues/12824#issuecomment-2563095382
+const cloudflareViteConfig = {
+  resolve: {
+    // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+    // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+    alias: import.meta.env.PROD && {
+      "react-dom/server": "react-dom/server.edge",
+    },
+  },
+}
+
+const adapter = LOCAL_BUILD
+  ? { netlify, cloudflare, vercel }[PREFERRED_HOSTING]()
+  : (NETLIFY_BUILD && netlify()) ||
+  (CLOUDFLARE_BUILD && cloudflare(cloudflareOptions)) ||
+  (VERCEL_BUILD && vercel());
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), markdoc(), keystatic()],
+  adapter,
+  vite: {
+    ...((LOCAL_BUILD && PREFERRED_HOSTING === 'cloudflare') || CLOUDFLARE_BUILD) ? cloudflareViteConfig : {}
+  }
+});
---
+++ keystatic.config.ts
@@ -0,0 +1,44 @@
+import { config, fields, collection, type LocalConfig, type GitHubConfig } from '@keystatic/core';
+
+const VERCEL_GIT_REPO_OWNER = import.meta.env.PUBLIC_VERCEL_GIT_REPO_OWNER;
+const VERCEL_GIT_REPO_SLUG = import.meta.env.PUBLIC_VERCEL_GIT_REPO_SLUG;
+const VERCEL_REPO = VERCEL_GIT_REPO_OWNER && VERCEL_GIT_REPO_SLUG && `${VERCEL_GIT_REPO_OWNER}/${VERCEL_GIT_REPO_SLUG}`;
+const REPO = import.meta.env.PUBLIC_REPO || VERCEL_REPO || '';
+const PUBLIC_KEYSTATIC_STORAGE_LOCAL = (
+  import.meta.env.PUBLIC_KEYSTATIC_STORAGE_LOCAL === true ||
+  import.meta.env.PUBLIC_KEYSTATIC_STORAGE_LOCAL === 'true' ||
+  (import.meta.env.PUBLIC_KEYSTATIC_STORAGE_LOCAL !== 'false' && (import.meta.env.DEV || false))
+);
+
+const localMode: LocalConfig['storage'] = {
+  kind: 'local',
+};
+
+const githubMode: GitHubConfig['storage'] = {
+  kind: 'github',
+  repo: REPO
+};
+
+export default config({
+  storage: PUBLIC_KEYSTATIC_STORAGE_LOCAL ? localMode : githubMode,
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'src/content/posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.markdoc({
+          label: 'Content',
+          options: {
+            image: {
+              directory: 'src/assets/images/posts',
+              publicPath: '../../assets/images/posts/',
+            },
+          },
+        }),
+      },
+    }),
+  },
+});
